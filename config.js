544fdc4190ed815c75c42af3a0731da1e38d58d1
fp <67727704+cugin2777@users.noreply.github.com>
2021-09-28 18:27:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'iu Linh iu Linh iu Linh ❤️❤️',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://facebook.com/nnnguyennn' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
